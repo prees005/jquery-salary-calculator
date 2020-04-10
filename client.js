154bca7ff4df5ff0d2a67e12f48a28624dab3d83
Sarah Preese <prees005@umn.edu>
2020-04-10 16:04:10 -0600

    Created addEmployee() function, which takes user inputs and stores employee info as an object.

--- client.js
+++ client.js
@@ -1,11 +1,30 @@
 $(document).ready(readyNow);
 
+let allEmployees = [];
+
 function readyNow() {
     //console.log('JQ READY');
     $('#submitInfo').on('click', addEmployee);
 }
 
 function addEmployee() {
+    
     let firstNameEmployee = $('#firstName').val();
-    let lastNameEmployee = 
+    let lastNameEmployee = $('#lastName').val();
+    let idNumberEmployee = $('#idNumber').val();
+    let jobTitleEmployee = $('#jobTitle').val();
+    let annualSalaryEmployee = $('#annualSalary').val();
+
+    let employeeObject = {
+        fName: firstNameEmployee,
+        lName: lastNameEmployee,
+        id: idNumberEmployee,
+        title: jobTitleEmployee,
+        salary: annualSalaryEmployee
+    };
+
+    allEmployees.push(employeeObject);
+
 }
+
+console.log(allEmployees);
