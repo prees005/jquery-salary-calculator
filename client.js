651babe0f530016e07fad0c4ff75429c0cf0c1ae
Sarah Preese <prees005@umn.edu>
2020-04-10 15:55:25 -0600

    Added base table to html

--- client.js
+++ client.js
@@ -1,5 +1,11 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('JQ READY');
+    //console.log('JQ READY');
+    $('#submitInfo').on('click', addEmployee);
+}
+
+function addEmployee() {
+    let firstNameEmployee = $('#firstName').val();
+    let lastNameEmployee = 
 }
